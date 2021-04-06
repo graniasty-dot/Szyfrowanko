1f80828c9e724fa57dc9c735e5ff89e1fba17caa
Tomasz <tomektendera@gmail.com>
2021-04-06 14:18:04 +0200

    Create cipher function prototypes.

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 import tkinter
 
 
+def fenceCipher(string=None, key=None):
+    """Funkcja szyfrujaca podany ciag znakow za pomoca szyfru ogrodzeniowego.
+
+    Args:
+        string (str, optional): Ciag znakow do szyfrowania. Defaults to None.
+        key (int, optional): Klucz szyfrujacy. Defaults to None.
+    """
+    pass
+
+def fenceDecipher():
+    pass
+
+def columnTransCipher(string=None, key=None):
+    """Funkcja szyfrujaca podany ciag znakow za pomoca transpozycji kolumnowej.
+
+    Args:
+        string (str, optional): Ciag znakow do szyfrowania. Defaults to None.
+        key (int, optional): Klucz szyfrujacy. Defaults to None.
+    """
+    pass
+
+def columnTransDecipher():
+    pass
+
+def lookForCipher():
+    pass
+
 
 if __name__ == "__main__":
     pass
