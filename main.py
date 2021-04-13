901e266816677b2246dec9a24e2a81ceb19fd787
Paweł Szczepka <pawel.szczepka@student.pk.edu.pl>
2021-04-13 17:55:19 +0200

    commit

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 import tkinter
-
-
+import cmath
+    #DZIAŁA ZROBIONE SZYFROWANIE
 def fenceCipher(string=None, key=None):
     """Funkcja szyfrujaca podany ciag znakow za pomoca szyfru ogrodzeniowego.
-
+    
     Args:
         string (str, optional): Ciag znakow do szyfrowania. Defaults to None.
         key (int, optional): Klucz szyfrujacy. Defaults to None.
-    """
-    pass
+    """ 
+    length=len(string)
+    #podział na wiersze
+    #generujemy nowe wiersze
+    rows=["" for i in range(key)]
+        #wpisujemy do listy
+    print(rows)
+    for x in range(length):
+     #tutaj ogarnąć wzór na wpisywanie 
+        rows[abs((key-1)-abs((key-1)-abs(x%((key-1)*2))))]+=string[x]
 
-def fenceDecipher():
+    print(rows)
+    encodedstring=""
+    for x in range(key):
+        encodedstring+=rows[x]
+    return encodedstring
+    pass
+#w trakcie prac
+def fenceDecipher(string=None, key=None):
+    length=len(string)
+    rows=["" for i in range(key)]
+    for x in range(length):
+        rows[abs((key-1)-abs((key-1)-abs(x%((key-1)*2))))]+=string[x]
+    print(rows)
     pass
 
 def columnTransCipher(string=None, key=None):
@@ -30,4 +50,9 @@ def lookForCipher():
 
 
 if __name__ == "__main__":
+
+    encoded=fenceCipher('ALAMAKOTAAKOTMAALE',4)
+    print(encoded)
+    decoded= fenceDecipher(encoded,4)
+    print(decoded)
     pass
