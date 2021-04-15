fa44cdac28c1d92fbe5dd6f9c8ca67781d7ea665
graniasty-dot <82032679+graniasty-dot@users.noreply.github.com>
2021-04-15 20:14:16 +0200

    Dodane szyfrowanie szyfrem kolumnowym

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter
 import cmath
 import string
+import math
+import random
     #DZIAŁA 
 def fenceCipher(string=None, key=None):
     """Funkcja szyfrujaca podany ciag znakow za pomoca szyfru ogrodzeniowego.
@@ -14,12 +16,10 @@ def fenceCipher(string=None, key=None):
     #generujemy nowe wiersze
     rows=["" for i in range(key)]
         #wpisujemy do listy
-    print(rows)
     for x in range(length):
      #tutaj ogarnąć wzór na wpisywanie 
         rows[abs((key-1)-abs((key-1)-abs(x%((key-1)*2))))]+=string[x]
 
-    print(rows)
     encodedstring=""
     for x in range(key):
         encodedstring+=rows[x]
@@ -52,7 +52,7 @@ def fenceDecipher(string=None, key=None):
     pass
 
 
-    #TODO:columnTransCipher(string=None, key=None):
+    #DZIAŁĄ:
 def columnTransCipher(string=None, key=None):
     """Funkcja szyfrujaca podany ciag znakow za pomoca transpozycji kolumnowej.
 
@@ -60,8 +60,35 @@ def columnTransCipher(string=None, key=None):
         string (str, optional): Ciag znakow do szyfrowania. Defaults to None.
         key (int, optional): Klucz szyfrujacy. Defaults to None.
     """
+    #Utworzenie wiersza szyfrującego
+    seq = [x for x in key],[x for x in range(len(key))]
 
+    for x in range(len(key)):
+        for y in range(x,len(key)):
+            print (seq[0][x],seq[0][y])
+            if seq[0][x]>seq[0][y]:
+                #swap podmiana wierszy
+                seq[1][x],seq[1][y]=seq[1][y],seq[1][x]
 
+    #podział na wiersze
+    rowsnumber=math.ceil(len(string)/len(key))
+    matrix= ['' for x in range(len(key))]
+    ans= ['' for x in range(len(key))]
+    
+    for x in range(len(string)):
+        matrix[x%len(key)]=''.join((matrix[x%len(key)],string[x]))
+
+    #Dopełnienie wolnych miejsc
+    for x in range(len(key)):
+        if len(matrix[x])!=rowsnumber:
+            matrix[x]= ''.join((matrix[x],random.choice(string.upper())))
+    
+    #Zamiana kolumn
+    print(seq)
+    for x in range(len(key)):
+        ans[seq[1][x]]= matrix[x]
+    
+    return "".join(str (x) for x in ans)
     pass
 
     #TODO: columnTransDecipher():
@@ -83,4 +110,6 @@ if __name__ == "__main__":
     print(encoded)
     decoded= fenceDecipher(encoded,4)
     print(decoded)
+    encoded = columnTransCipher(anystring,'KABANOS')
+    print(encoded)
     pass
