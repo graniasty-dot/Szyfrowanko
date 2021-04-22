34e225a37334a16fcf6345177fd7ba0ad3c8498a
Tomek Tendera <tomektendera@gmail.com>
2021-04-23 00:10:11 +0200

    Add 'about' pop-up window Add prototype function to import/export data

--- gui.py
+++ gui.py
@@ -9,6 +9,7 @@ class MainWindow(tk.Tk):
         self.version = "v1.0"
         self.window_title = "Szyfrowanie - ISTU"
         self.title(self.window_title + " " + self.version)
+        self.resizable(width=False, height=False)
         self.menu = tk.Menu(self, tearoff=0)
 
         filemenu = tk.Menu(self.menu, tearoff=0)
@@ -18,12 +19,10 @@ class MainWindow(tk.Tk):
 
         aboutmenu = tk.Menu(self.menu, tearoff=0)
         aboutmenu.add_command(label="Instrukcja")
-        aboutmenu.add_command(label="Autorzy")
+        aboutmenu.add_command(label="O programie", command=self.showAbout)
         self.menu.add_cascade(label="Pomoc", menu=aboutmenu)
 
         self.config(menu=self.menu)
-        # self.aboutmenu = tk.Menu(self.menubar, tearoff=0)
-        # self.menubar.add_cascade(label="Pomoc", menu=self.menubar)
 
         # Ramki na elementy
         self.inputFrame = tk.Frame(self)
@@ -88,10 +87,35 @@ class MainWindow(tk.Tk):
         )
         self.submitButton.pack(padx=5, pady=5)
 
+    def showAbout(self):
+        text = """Aplikacja służy do szyfrowania i odszyfrowywania wiadomości korzystając z szyfru ogrodzeniowego lub szyfru transpozycji kolumnowej.\nAby dowiedzieć się jak korzystać z aplikacji naciśnij 'Instrukcja' w menu 'Pomoc' """
+        window = tk.Toplevel(self)
+        window.geometry("300x200")
+        window.resizable(width=False, height=False)
+        window.title("O programie")
+        tk.Label(window, text=self.window_title, font="Helvetica 11 bold").pack(
+            padx=6, pady=(3, 10), anchor="center"
+        )
+        tk.Message(window, text=text, aspect=300).pack(padx=4, pady=(0, 6), fill=tk.X)
+        tk.Label(
+            window,
+            text="Autorzy: Paweł Szczepka, Tomasz Tendera",
+            font="Helvetica 10 italic",
+        ).pack(padx=3, pady=(3, 6), anchor=tk.CENTER)
+        tk.Button(window, text="Ok", width=20, command=window.destroy).pack(
+            padx=3, pady=5
+        )
+
     def delPlaceholder(self, event):
         message = event.widget.get(1.0, tk.END)
         if message == "Tekst podstawowy\n" or message == "Tekst zaszyfrowany\n":
             event.widget.delete(1.0, tk.END)
+
+    def importFromFile(self):
+        pass
+
+    def exportToFile(self):
+        pass
 
     def buttonClicked(self):
         # TODO: Sprawdzanie poprawnosci wprowadzania danych, konwersja na wielkie/male litery
